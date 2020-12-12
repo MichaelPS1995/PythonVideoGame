00b2899ddbb0cd0e642ca51d06e0abfe68f518bf
MichaelPS1995 <42724257+MichaelPS1995@users.noreply.github.com>
2020-12-11 22:02:18 -0700

    Update SpongeBobGame.py

--- SpongeBobGame.py
+++ SpongeBobGame.py
@@ -1,6 +1,7 @@
 #This was saved as a .ipynb but that's not supported by git so i did python to highlight syntax and so it would load the code.
 
 
+
 #########################################
 
 def get_distance(enemy_x, enemy_y, player_x, player_y):
@@ -13,7 +14,7 @@ def get_distance(enemy_x, enemy_y, player_x, player_y):
 #########################################
 
 def move(x_pos, y_pos, dist, grid, grid_size, sq_num):
-     
+    
     row,col = get_cell(grid_size,sq_num,x_pos,y_pos)
     
     sq_size = grid_size/sq_num
@@ -27,7 +28,7 @@ def move(x_pos, y_pos, dist, grid, grid_size, sq_num):
     tr = (tr[0] - .0001, tr[1] + .0001)
     br = (br[0] - .0001, br[1] - .0001)
     
-    if event.key == pygame.K_LEFT:
+    if (event.key == pygame.K_LEFT):
         # Check to see if a left keystroke will result in movement. If the player will be off screen as a result of the 
         # key press, do not advance the x coordinate. If the player will be moving through a barrier, do not advance the
         # x coordinate. If the player will not move into an obstruction or off screen, advance the x coordinate (accordingly)
@@ -38,7 +39,7 @@ def move(x_pos, y_pos, dist, grid, grid_size, sq_num):
             if (grid[row_tl][col_tl] != 1) and (grid[row_bl][col_bl] != 1):
                 x_pos -= dist
         
-    if event.key == pygame.K_RIGHT:
+    if (event.key == pygame.K_RIGHT):
         # Repeat, as above, but for the right key press
         row_tr, col_tr = get_cell(grid_size, sq_num, tr[0] + dist, tr[1])
         row_br, col_br = get_cell(grid_size, sq_num, br[0] + dist, br[1])
@@ -46,7 +47,7 @@ def move(x_pos, y_pos, dist, grid, grid_size, sq_num):
             if(grid[row_tr][col_tr] != 1) and (grid[row_br][col_br] != 1):
                 x_pos += dist
     
-    if event.key == pygame.K_UP:
+    if (event.key == pygame.K_UP):
         
         # Repeat, as above, but for the up key press
         row_tl, col_tl = get_cell(grid_size, sq_num, tl[0], tl[1] - dist)
@@ -55,7 +56,7 @@ def move(x_pos, y_pos, dist, grid, grid_size, sq_num):
             if(grid[row_tl][col_tl] != 1) and (grid[row_tr][col_tr] != 1):
                 y_pos -= dist
         
-    if event.key == pygame.K_DOWN:
+    if (event.key == pygame.K_DOWN):
         
         # Repeat, as above, but for the down key press
         row_bl, col_bl = get_cell(grid_size, sq_num, bl[0], bl[1] + dist)
@@ -70,7 +71,7 @@ def move(x_pos, y_pos, dist, grid, grid_size, sq_num):
 #########################################
 
 def enemy_move(enemy_x, enemy_y, player_x, player_y, dist, grid, grid_size, sq_num):
-    
+
     #grabs location
     sq_size = grid_size/sq_num
     tl = (enemy_x,enemy_y)
@@ -100,7 +101,7 @@ def enemy_move(enemy_x, enemy_y, player_x, player_y, dist, grid, grid_size, sq_n
         row_bl, col_bl = get_cell(grid_size, sq_num, bl[0] - dist, bl[1])
         if(enemy_x>0):
             if (grid[row_tl][col_tl] != 1) and (grid[row_bl][col_bl] != 1):
-                enemy_x -= (dist/5)
+                enemy_x -= (dist/2)
                 
     #right
     if (min(l_m,r_m,u_m,d_m) == r_m): 
@@ -109,7 +110,7 @@ def enemy_move(enemy_x, enemy_y, player_x, player_y, dist, grid, grid_size, sq_n
         row_br, col_br = get_cell(grid_size, sq_num, br[0] + dist, br[1])
         if(x_pos+sq_size < grid_size):
             if(grid[row_tr][col_tr] != 1) and (grid[row_br][col_br] != 1):
-                enemy_x += (dist/5)
+                enemy_x += (dist/2)
     #up
     if (min(l_m,r_m,u_m,d_m) == u_m):     
         #print("up")
@@ -117,7 +118,7 @@ def enemy_move(enemy_x, enemy_y, player_x, player_y, dist, grid, grid_size, sq_n
         row_tr, col_tr = get_cell(grid_size, sq_num, tr[0], tr[1] - dist)
         if(enemy_y > 0):
               if(grid[row_tl][col_tl] != 1) and (grid[row_tr][col_tr] != 1):
-                enemy_y -= (dist/5)
+                enemy_y -= (dist/2)
                 
     #down
     if (min(l_m,r_m,u_m,d_m) == d_m): 
@@ -126,7 +127,7 @@ def enemy_move(enemy_x, enemy_y, player_x, player_y, dist, grid, grid_size, sq_n
         row_br, col_br = get_cell(grid_size, sq_num, br[0], br[1] + dist)
         if(enemy_y+sq_size < grid_size):
             if(grid[row_bl][col_bl] != 1 and grid[row_br][col_br] != 1):
-                enemy_y += (dist/5)
+                enemy_y += (dist/2)
     
     return enemy_x, enemy_y
 
@@ -271,7 +272,7 @@ enemy1_y = enemy1_start_cell[1]*SQ_SIZE  # because get cell is reversed
 enemy2_x = enemy2_start_cell[0]*SQ_SIZE
 enemy2_y = enemy2_start_cell[1]*SQ_SIZE
 
-#** Define treasure locations, only 3
+#** Define treasure locations, only 3, and crabby patty location, only 1
            
 tr1_cell = random.choice(empty_cells)            
 tr2_cell = random.choice(empty_cells)            
@@ -280,13 +281,18 @@ if(tr2_cell == tr1_cell):
 tr3_cell = random.choice(empty_cells)
 if(tr3_cell == tr1_cell or tr3_cell == tr2_cell):
     tr3_cell = random.choice(empty_cells)
-    
+crabby_patty = random.choice(empty_cells)
+if(crabby_patty == tr1_cell or crabby_patty == tr2_cell or crabby_patty == tr3_cell):
+    crabby_patty = random.choice(empty_cells)
+
 tr1_x = tr1_cell[0]*SQ_SIZE
 tr1_y = tr1_cell[1]*SQ_SIZE
 tr2_x = tr2_cell[0]*SQ_SIZE
 tr2_y = tr2_cell[1]*SQ_SIZE
 tr3_x = tr3_cell[0]*SQ_SIZE
 tr3_y = tr3_cell[1]*SQ_SIZE
+patty_x = crabby_patty[0]*SQ_SIZE
+patty_y = crabby_patty[1]*SQ_SIZE
 
 #print(grid)
 #print(empty_cells)
@@ -302,13 +308,15 @@ tr2_col,tr2_row = tr2_cell
 grid[tr2_col][tr2_row] = 2
 tr3_col,tr3_row = tr3_cell
 grid[tr3_col][tr3_row] = 2
+cp_col,cp_row = crabby_patty
+grid[cp_col][cp_row] = 3
 #print(grid)
 
 # boolean truth conditions:
 tr1 = True
 tr2 = True
 tr3 = True
-
+cp = True
 
 ####################################################
 # Change FILENAME.png to the file name of your image. Note that .jpg and .gif will also work
@@ -323,6 +331,8 @@ barrier_im = pygame.transform.scale(barrier_im, (SQ_SIZE, SQ_SIZE))
 #** treasure image - squidwards clarinet:
 treasure_im = pygame.image.load("clarinet.png")
 treasure_im = pygame.transform.scale(treasure_im, (SQ_SIZE, SQ_SIZE))
+crabby_patty_im = pygame.image.load("crabbypatty.png")
+crabby_patty_im = pygame.transform.scale(crabby_patty_im, (SQ_SIZE,SQ_SIZE))
 
 # initialize score scree printout
 font = pygame.font.Font('freesansbold.ttf', 30) # sets the font and size of the text 
@@ -373,7 +383,7 @@ while True:
             enemy1_start_cell = random.choice(empty_cells)            
             enemy2_start_cell = random.choice(empty_cells)
             if(enemy2_start_cell == enemy1_start_cell):
-                enemy2_start_cell = random.choice(empty_cells)    
+                enemy2_start_cell = random.choice(empty_cells)
             enemy1_x = enemy1_start_cell[0]*SQ_SIZE  # had to add 1, and reverse order
             enemy1_y = enemy1_start_cell[1]*SQ_SIZE  # because get cell is reversed
             enemy2_x = enemy2_start_cell[0]*SQ_SIZE
@@ -388,13 +398,18 @@ while True:
             tr3_cell = random.choice(empty_cells)
             if(tr3_cell == tr1_cell or tr3_cell == tr2_cell):
                 tr3_cell = random.choice(empty_cells)
+            crabby_patty = random.choice(empty_cells)
+            if(crabby_patty == tr1_cell or crabby_patty == tr2_cell or crabby_patty == tr3_cell):
+                crabby_patty = random.choice(empty_cells)
             tr1_x = tr1_cell[0]*SQ_SIZE
             tr1_y = tr1_cell[1]*SQ_SIZE
             tr2_x = tr2_cell[0]*SQ_SIZE
             tr2_y = tr2_cell[1]*SQ_SIZE
             tr3_x = tr3_cell[0]*SQ_SIZE
             tr3_y = tr3_cell[1]*SQ_SIZE
-            
+            patty_x = crabby_patty[0]*SQ_SIZE
+            patty_y = crabby_patty[1]*SQ_SIZE ####################################################
+            print(tr1_cell)
             #print(grid)
             #print(empty_cells)
             #print(tr1_cell)
@@ -409,12 +424,15 @@ while True:
             grid[tr2_col][tr2_row] = 2
             tr3_col,tr3_row = tr3_cell
             grid[tr3_col][tr3_row] = 2
+            cp_col,cp_row = crabby_patty
+            grid[cp_col][cp_row] = 3
             #print(grid)
             
             # boolean truth conditions:
             tr1 = True
             tr2 = True
             tr3 = True
+            cp = True
             ####################################################
         
         p_tl, p_tr, p_bl, p_br = corner_points(GRID_SIZE, SQ_NUM, x_pos, y_pos)
@@ -457,7 +475,6 @@ while True:
         check1 = col1,row1
         check2 = col2,row2
         check3 = col3,row3
-        
         #print(check)
         #print(col,row)
         #print(grid[col][row])
@@ -476,7 +493,11 @@ while True:
             if SCORE == 3:
                 pygame.display.set_caption('PRESS ANY ARROW-KEY TO CONTINUE')
             #print(SCORE)
-            
+        if (grid[col][row] == 3 or grid[col1][row1] == 3 or grid[col2][row2] == 3 or grid[col3][row3] == 3):
+            if (check == crabby_patty or check1 == crabby_patty or check2 == crabby_patty or check3 == crabby_patty):
+                cp = False
+                grid[cp_col][cp_row] = 0
+                LIVES += 1
         # Update Score:
         text = 'Score: '+str(SCORE) 
         text = font.render(text, True, (50,50,50), (100,100,100)) #This renders the text
@@ -499,14 +520,16 @@ while True:
         
         # Check to see if a button has been pressed.
         # If an arrow key has been pressed, call the move() function and determine new x and y coordinates.
-        if event.type == pygame.KEYDOWN:
-            
+        pygame.event.set_blocked(pygame.MOUSEMOTION)
+        pygame.event.set_blocked(pygame.MOUSEBUTTONDOWN)
+        pygame.event.set_blocked(pygame.MOUSEBUTTONUP)
+        
+        if (event.type == pygame.KEYDOWN):
             x_pos,y_pos = move(x_pos, y_pos, 5, grid, GRID_SIZE, SQ_NUM)
-    
+
         #Get new coordinates the enemies will move to
         enemy1_x, enemy1_y = enemy_move(enemy1_x, enemy1_y, x_pos, y_pos, 1, grid, GRID_SIZE, SQ_NUM)
         enemy2_x, enemy2_y = enemy_move(enemy2_x, enemy2_y, x_pos, y_pos, 1, grid, GRID_SIZE, SQ_NUM)
-             
         # Draw player and both enemy images to the screen using the blit() function
         DISPLAYSURF.blit(player_im, (x_pos,y_pos))
         DISPLAYSURF.blit(enemy_im, (enemy1_x, enemy1_y))
@@ -517,6 +540,8 @@ while True:
             DISPLAYSURF.blit(treasure_im, (tr2_x, tr2_y))
         if (tr3):
             DISPLAYSURF.blit(treasure_im, (tr3_x, tr3_y))
+        if (cp):
+            DISPLAYSURF.blit(crabby_patty_im, (patty_x, patty_y))
         DISPLAYSURF.blit(text,textRect)
         DISPLAYSURF.blit(text1,textRect1)
 
